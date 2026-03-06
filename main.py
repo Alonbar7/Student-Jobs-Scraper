f13666c6455bbb9668a4248b7efc670b64d1abf4
Alonbar7 <bar.alon.111@gmail.com>
2026-03-06 02:04:25 +0200

    updated functionality

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from Scraper import *
 from MessageBot import MessageBot
-
 import os
-from dotenv import load_dotenv
 
 def main():
 
@@ -11,10 +9,14 @@ def main():
     jobs = jobFinder.fetch_linkedin_jobs()
     
     # Get bot token and chat id
-    load_dotenv()
     token = os.environ["TELEGRAM_BOT_TOKEN_STUDENT_JOBS"]
     chat_id = os.environ["TELEGRAM_CHAT_ID_STUDENT_JOBS"]
     bot = MessageBot(token, chat_id)
+    
+    # Get db username and password
+    username = os.environ["MONGODB_USERNAME"]
+    password = os.environ["MONGODB_PASSWORD"]
+    url = f"mongodb+srv://{username}:{password}@student-jobs.fw1npmz.mongodb.net/?appName=student-jobs"
     
     for job in jobs:
         bot.send_message(str(job))
