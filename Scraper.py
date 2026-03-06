f7380709fade817484ff37466af275d12321431f
Alonbar7 <bar.alon.111@gmail.com>
2026-03-06 02:22:09 +0200

    added infinite scrolling and searching by date

--- Scraper.py
+++ Scraper.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import time
+import random
 import re
 
 # Job container
@@ -23,7 +25,7 @@ class JobFinder:
         self.location = location
         self.positions = positions
 
-    def construct_link(self):        
+    def construct_link(self, start = 0):        
         if not self.positions:
             return None
 
@@ -35,67 +37,82 @@ class JobFinder:
 
         # Construct url
         # url = "https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=Data+Engineer&location=Israel&f_E=1,2&f_JT=P,I&start=0"
-        url = f"https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords={search}&location={self.location}&f_E=1,2&f_JT=P,I&start=0"
+        url = f"https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords={search}&location={self.location}&f_E=1,2&f_JT=P,I&sortBy=DD&start={start}"
 
         return url
 
     def fetch_linkedin_jobs(self):
-        url = self.construct_link()
+        # Url page counter
+        start = 0
 
         # Browswer agent
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
         }
         
-        try:
-            # Request Guest API
-            response = requests.get(url, headers=headers)
+        while True:
+            try:
+                # Create url
+                url = self.construct_link(start)
+                
+                # Request Guest API
+                response = requests.get(url, headers=headers)
 
-            if response.status_code != 200:
-                print(f"API request failed. Status code: {response.status_code}")
+                if response.status_code != 200:
+                    print(f"API request failed. Status code: {response.status_code}")
+                    return None
+                else:
+                    print("API Request Worked!")
+
+                # Proccessing the information
+                soup = BeautifulSoup(response.text, 'html.parser')
+                
+                # Get job tabs from the result
+                job_cards = soup.find_all('div', class_='base-card')        
+                if not job_cards:
+                    job_cards = soup.find_all('li')
+                    
+                # Break loop if reached an empty page
+                if not job_cards:
+                    break
+
+                # Save jobs
+                self.jobs = []
+                self.job_number = len(job_cards)
+
+                # Get elements
+                for index, card in enumerate(job_cards, start=1):
+                    try:
+                        title_elem = card.find('h3', class_='base-search-card__title')
+                        title = title_elem.get_text(strip=True) if title_elem else "N/A"
+                        
+                        company_elem = card.find('h4', class_='base-search-card__subtitle')
+                        company = company_elem.get_text(strip=True) if company_elem else "N/A"
+                        
+                        link_elem = card.find('a', class_='base-card__full-link')
+                        job_link = link_elem['href'] if link_elem and 'href' in link_elem.attrs else "No Link"
+                        
+                        clean_link = job_link.split('?')[0]
+                        
+                        regex_id = re.search(r'\d{9,10}', clean_link)
+                        if not regex_id: continue
+                        job_id = regex_id.group(0)
+                        
+                        job = Job(job_id, title, company, clean_link)
+                        self.jobs.append(job)
+
+                    except Exception as e:
+                        print(e)
+                        return self.jobs if self.jobs else None
+                
+                # Go to the next page
+                start += len(job_cards)
+                
+                # Rest to counter block
+                time.sleep(random.uniform(2.0, 4.0))
+            except Exception:
+                print("Failuer Exceuting")
                 return None
-            else:
-                print("API Request Worked!")
-
-            # Proccessing the information
-            soup = BeautifulSoup(response.text, 'html.parser')
-            
-            # Get job tabs from the result
-            job_cards = soup.find_all('div', class_='base-card')        
-            if not job_cards:
-                job_cards = soup.find_all('li')
-
-            # Save jobs
-            self.jobs = []
-            self.job_number = len(job_cards)
-
-            # Get elements
-            for index, card in enumerate(job_cards, start=1):
-                try:
-                    title_elem = card.find('h3', class_='base-search-card__title')
-                    title = title_elem.get_text(strip=True) if title_elem else "N/A"
-                    
-                    company_elem = card.find('h4', class_='base-search-card__subtitle')
-                    company = company_elem.get_text(strip=True) if company_elem else "N/A"
-                    
-                    link_elem = card.find('a', class_='base-card__full-link')
-                    job_link = link_elem['href'] if link_elem and 'href' in link_elem.attrs else "No Link"
-                    
-                    clean_link = job_link.split('?')[0]
-                    
-                    regex_id = re.search(r'\d{9,10}', clean_link)
-                    if not regex_id: continue
-                    job_id = regex_id.group(0)
-                    
-                    job = Job(job_id, title, company, clean_link)
-                    self.jobs.append(job)
-
-                except Exception as e:
-                    print(e)
-                    return self.jobs if self.jobs else None
-        except Exception:
-            print("Failuer Exceuting")
-            return None
         
         return self.jobs
     
